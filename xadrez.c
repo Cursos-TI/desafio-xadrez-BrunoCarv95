976b34470254a9a474bb2db3c22f652bfa83ab45
BrunoCarv95 <brunohero007@gmail.com>
2025-06-02 02:39:28 +0000

    Desafio de Xadrez Aventureiro

--- xadrez.c
+++ xadrez.c
@@ -4,6 +4,7 @@
 #define MOV_TORRE 5
 #define MOV_BISPO 5
 #define MOV_RAINHA 8
+#define MOV_CAVALO 1
 
 int main() {
     int i;
@@ -30,6 +31,17 @@ int main() {
         printf("Esquerda\n");
         i++;
     } while (i < MOV_RAINHA);
+
+    // Movimentação do CAVALO: 3 movimentos em L (2 Baixo + 1 Esquerda)
+    printf("\nMovimentação do Cavalo:\n");
+    for (i = 0; i < MOV_CAVALO; i++) {
+        int j = 0;
+        while (j < 2) {
+            printf("Baixo\n");
+            j++;
+        }
+        printf("Esquerda\n");
+    }
 
     return 0;
 }
